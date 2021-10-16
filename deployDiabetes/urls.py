0fbe4872160e874fb7451a544106b6917b248a35
hamzalahkim <hamza.lahkim@emines.um6p.ma>
2021-10-16 17:36:04 +0100

    first most basic model dev

--- deployDiabetes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from pima import views
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('model/', views.call_model.as_view()) # "we use as as_view function because in Class-based views, we have to call as_view() function so as to return a callable view that takes a request and returns a response."
-]
